c68748f298f637a0fb463952b2badf834605144e
saahub <carolina-saavedra1@hotmail.com>
2017-11-12 23:39:49 -0300

    entrega u.u

---
+++ app.js
@@ -0,0 +1,24 @@
+function dontShow(){
+    document.getElementById('dontShow').style.display = 'none';
+}
+
+function show(){
+    document.getElementById('formulario').style.display = 'block';
+}
+
+
+(function(){
+
+
+
+var tablero = document.getElementById("tablero");
+var inside = document.getElementById("inside");
+var escondido =document.getElementById("escondido"); 
+
+
+addList.addEventListener("click", function(){
+
+
+
+})
+
